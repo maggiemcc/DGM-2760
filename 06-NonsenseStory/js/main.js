d079efbaee4eff47c40604b6acde0c2e92e863ff
Maggie McCausland <maggiemccausland@hotmail.com>
2020-09-26 10:08:44 -0600

    made slight change to the string story.

--- 06-NonsenseStory/js/main.js
+++ 06-NonsenseStory/js/main.js
@@ -29,7 +29,7 @@ function tellStory() {
 
     const myStory = `Once upon a time there were four <span>${adjectiveArray[0]}</span> <span>${nounArray[0]}s</span> named Romper, Dixie, Dazzle, and Rockefeller.
     They lived alone with a <span>${nounArray[1]}</span> named Mrs. Hollow. She is <span>${adjectiveArray[1]}</span> as can be due to all the <span>${nounArray[2]}</span> she <span>${verbArray[0]}s</span> each night in her <span>${adjectiveArray[2]} ${nounArray[3]}</span>.
-    Now, my dears, said Mrs. Hollow, you may go over the <span>${nounArray[4]}</span> or around the <span>${nounArray[5]}</span>, but do not go into the <span>${nounArray[6]}’s</span> layer. The last <span>${nounArray[6]}</span> incident has caused enough problems for me and I don’t need another.
+    Now, my dears, said Mrs. Hollow, you may go over the <span>${nounArray[4]}</span> or around the <span>${nounArray[5]}</span>, but do not go into the <span>${nounArray[6]}’s</span> layer. The last <span>${nounArray[6]}</span> incident has caused enough problems for me, and I don’t need another.
     Now <span>${verbArray[1]}</span> along, and don't get into mischief. I am going to do some <span>${verbArray[2]}ing</span> while you are all away.`
 
     // get a reference to the querySelector('#story') = myStory
